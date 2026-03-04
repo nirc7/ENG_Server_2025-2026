1e9302fc02b5bfb845f67b034c3721d407f7ab27
nc <31476486+nirc7@users.noreply.github.com>
2026-03-04 11:33:00 +0200

    ds

---
+++ Lec03 DS GV/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string conStr = "...";
+            SqlConnection con  = new SqlConnection(conStr);
+            DataSet ds = new DataSet();
+            SqlDataAdapter adptr = new SqlDataAdapter(
+                "SELECT * FROM TBUsers", con);
+            adptr.Fill(ds,"Users");
+            DataTable dtUsers =  ds.Tables["Users"];
+            dtUsers.Rows[2]["Name"] = "ben";
+
+        }
+    }
+}
