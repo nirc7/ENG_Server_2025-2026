8a46741f20500208fa124b3cee67ccc09422fd35
nc <31476486+nirc7@users.noreply.github.com>
2026-06-18 13:39:08 +0300

    somme

--- Lec04 Client Server Connection - WEB API/aspDemoNet8/WebApplication1/WebApplication1/Models/DBStudentsSQL.cs
+++ Lec04 Client Server Connection - WEB API/aspDemoNet8/WebApplication1/WebApplication1/Models/DBStudentsSQL.cs
@@ -10,8 +10,9 @@ namespace WebApplication1.Models
         {
             
             List<Student> students = new List<Student>();
-            //string strCon = @"Data Source=LAB-G700;Initial Catalog=DBUsers;User ID=sa;Password=RuppinTech!;";
-            string strCon = @"Data Source=sql.bsite.net\MSSQL2016;User ID=kebewol829_;Password=RuppinTech!;Pooling=False;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Authentication=RuppinTech!;";
+            //string strCon = @"Data Source=HOME;Initial Catalog=DBUsers;Integrated Security=True;Trust Server Certificate=True";
+            string strCon = @"workstation id=ruppin1.mssql.somee.com;packet size=4096;user id=rup1_SQLLogin_1;pwd=wi8b7cvrah;data source=ruppin1.mssql.somee.com;persist security info=False;initial catalog=ruppin1;TrustServerCertificate=True";
+            //string strCon = @"Data Source=sql.bsite.net\MSSQL2016;User ID=kebewol829_;Password=RuppinTech!;Pooling=False;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Authentication=RuppinTech!;";
             SqlConnection con = new SqlConnection(strCon);
 
             SqlCommand com = new SqlCommand(
